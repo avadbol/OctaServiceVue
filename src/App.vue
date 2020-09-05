5a2a25ac52af6d93fdda4c79ca008423fdbf5747
A. Adnan Bolaman <avniadnanbolaman@aof.anadolu.edu.tr>
2020-09-05 13:20:12 +0300

    UPDATE

--- src/App.vue
+++ src/App.vue
@@ -1,16 +1,13 @@
 <template>
   <router-view></router-view>
 </template>
-
 <style lang="scss">
 @import "~bootstrap-vue/dist/bootstrap-vue.css";
 @import "~perfect-scrollbar/css/perfect-scrollbar.css";
 @import "~socicon/css/socicon.css";
 @import "~@fortawesome/fontawesome-free/css/all.css";
 @import "~line-awesome/dist/line-awesome/css/line-awesome.css";
-@import "assets/plugins/flaticon/flaticon.css";
-@import "assets/plugins/flaticon2/flaticon.css";
-@import "assets/plugins/keenthemes-icons/font/ki.css";
+
 // Main demo style scss
 @import "assets/sass/style.vue";
 
