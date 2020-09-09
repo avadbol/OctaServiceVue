e04239e00c3d7e5f9280b8751801d583fd61a890
avadbol <adnanbolaman@gmail.com>
2020-09-09 17:21:03 +0300

    update

---
+++ src/view/components/define/SelectStockUnit.vue
@@ -0,0 +1,33 @@
+<template>
+  <div>
+<!--    <b-form-select @input="$emit('input',$event.target.value)" :options="unitGetlist" :text-field="'name'" :value-field="'id'" :class="cLass"></b-form-select>-->
+<!--    {{unitGetlist}}-->
+
+    <select :class="classs" class="form-control" @input="$emit('input',$event.target.value)">
+      <option v-for="item in unitGetlist" :value="item.id" >{{item.name}}</option>
+    </select>
+  </div>
+
+</template>
+
+<script>
+import {mapGetters} from "vuex";
+
+export default {
+  props:['textField','valueField','classs'],
+  methods:{
+    inputSend(e){
+      console.log(e);
+      $emit('input',e.target.value)
+    }
+  },
+  computed:{
+    ...mapGetters(["unitGetlist"]),
+
+  },
+  created() {
+    this.$store.dispatch("initUnits");
+  }
+}
+</script>
+
---
+++ src/view/components/stock/SelectStockSearchList.vue
@@ -0,0 +1,13 @@
+<template>
+$END$
+</template>
+
+<script>
+export default {
+name: "SelectStockSearchList"
+}
+</script>
+
+<style scoped>
+
+</style>
